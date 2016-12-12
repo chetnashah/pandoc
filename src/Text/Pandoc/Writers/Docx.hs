09e73e612ad6ac52c6016881a475d16baa8e7261
John MacFarlane <jgm@berkeley.edu>
2016-12-12 13:51:20 +0100

    Text.Pandoc.Shared: Removed fetchItem, fetchItem'.
    Made changes where these are used, so that the version
    of fetchItem from PandocMonad can be used instead.

--- src/Text/Pandoc/Writers/Docx.hs
+++ src/Text/Pandoc/Writers/Docx.hs
@@ -48,6 +48,7 @@ import Text.Pandoc.Options
 import Text.Pandoc.Writers.Math
 import Text.Pandoc.Highlighting ( highlight )
 import Text.Pandoc.Walk
+import Text.Pandoc.Error (PandocError)
 import Text.Highlighting.Kate.Types ()
 import Text.XML.Light as XML
 import Text.TeXMath
@@ -55,10 +56,10 @@ import Text.Pandoc.Readers.Docx.StyleMap
 import Text.Pandoc.Readers.Docx.Util (elemName)
 import Control.Monad.Reader
 import Control.Monad.State
+import Control.Monad.Except (runExceptT)
 import Text.Highlighting.Kate
 import System.Random (randomR)
 import Text.Printf (printf)
-import qualified Control.Exception as E
 import Data.Monoid ((<>))
 import Text.Pandoc.MIME (MimeType, getMimeType, getMimeTypeDef,
                          extensionFromMimeType)
@@ -1179,10 +1180,10 @@ inlineToOpenXML' opts (Image attr alt (src, title)) = do
   case M.lookup src imgs of
     Just (_,_,_,elt,_) -> return [elt]
     Nothing -> do
-      res <- (lift . lift) $ P.fetchItem' (writerMediaBag opts) (writerSourceURL opts) src
+      res <- runExceptT $ lift (P.fetchItem (writerSourceURL opts) src)
       case res of
-        Left (_ :: E.SomeException) -> do
-          (lift . lift) $ P.warning ("Could not find image `" ++ src ++ "', skipping...")
+        Left (_ :: PandocError) -> do
+          P.warning ("Could not find image `" ++ src ++ "', skipping...")
           -- emit alt text
           inlinesToOpenXML opts alt
         Right (img, mt) -> do
